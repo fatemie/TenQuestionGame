3293e531d630f2eaf2e3db99dfb5874d4beb1715
fateme <fs.fatemi@chmail.ir>
2022-02-25 23:45:33 +0330

    configure change is managed

--- app/src/main/java/com/example/tenquestiongame/questionActivity.kt
+++ app/src/main/java/com/example/tenquestiongame/questionActivity.kt
@@ -4,10 +4,12 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.widget.Toast
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import com.example.tenquestiongame.databinding.ActivityQuectionBinding
+import java.lang.Boolean.getBoolean
 
 class questionActivity : AppCompatActivity() {
 
@@ -15,19 +17,23 @@ class questionActivity : AppCompatActivity() {
     var questionNumber = 0
     var question = Question()
     var answerPressed = false
-    var cheatList = arrayListOf(false, false,false)
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        question.data()
+        questionNumber = intent.getIntExtra("questionNumber", 0)
         binding = ActivityQuectionBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
-        question.data()
-        questionNumber = intent.getIntExtra("questionNumber", 0)
-      //  title = "Question $questionNumber"
-        setListener()
+        if(savedInstanceState != null){
+            binding.questionView.text = savedInstanceState.getString("questionView")
+            questionNumber = savedInstanceState.getInt("questionNumber")
+            question.questionList[questionNumber-1].isCheated = savedInstanceState.getBoolean("isCheat")
+            answerPressed = savedInstanceState.getBoolean("answerPressed")
 
+        }
+        setListener()
     }
 
 
@@ -37,9 +43,9 @@ class questionActivity : AppCompatActivity() {
             binding.prevbutton.isEnabled = false
         }
 
-        if(question.questionList[questionNumber].isCheated == false){
-            Toast.makeText(this, "you can use cheat", Toast.LENGTH_SHORT).show()
-        }
+//        if(question.questionList[questionNumber-1].isCheated == false){
+//            Toast.makeText(this, "you can use cheat", Toast.LENGTH_SHORT).show()
+//        }
 
 
         binding.truebutton.setOnClickListener {
@@ -64,6 +70,7 @@ class questionActivity : AppCompatActivity() {
     fun setQuestion(){
         binding.questionView.text = question.questionList[questionNumber-1].description
     }
+
 
     fun setTrueButton(){
         if(!answerPressed){
@@ -159,6 +166,16 @@ class questionActivity : AppCompatActivity() {
                 }
             }
         }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState?.run {
+            putString("questionView", binding.questionView.text.toString())
+            putBoolean("isCheat",question.questionList[questionNumber-1].isCheated )
+            putInt("questionNumber", questionNumber)
+            putBoolean("answerPressed", answerPressed)
+        }
+        super.onSaveInstanceState(outState)
     }
 
 }
